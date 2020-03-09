5c1a8e9102edcaef938b033ba13ed7ca2e575f9a
Anne SCHUKIN <anya.schukin@gmail.com>
2020-03-09 14:23:17 +0100

    feature/Colleen.c finished

--- Colleen.c
+++ Colleen.c
@@ -5,7 +5,7 @@
 */
 
 void print(char *source) {
-    printf(source,10,9,34,source);
+    printf(source,10,34,source);
 }
 
 int main() {
@@ -13,7 +13,7 @@ int main() {
     * here is my source code
     */
 
-    char *source="#include <stdio.h>%1$c%1$c/*%1$c* print my source code%1$c*/%1$c%1$cvoid print(char *source) {%1$c  printf(source,10,9,34,source);%1$c}%1$c%1$cint main() {%1$c  /*%1$c  * here is my source code%1$c  */%1$c%1$c  char *source=%2$c%4$s%2$c;%1$c  print(source);%1$c  return(0);%1$c}";
+    char *source="#include <stdio.h>%1$c%1$c/*%1$c* print my source code%1$c*/%1$c%1$cvoid print(char *source) {%1$c    printf(source,10,34,source);%1$c}%1$c%1$cint main() {%1$c    /*%1$c    * here is my source code%1$c    */%1$c%1$c    char *source=%2$c%3$s%2$c;%1$c    print(source);%1$c    return(0);%1$c}";
     print(source);
     return(0);
 }
