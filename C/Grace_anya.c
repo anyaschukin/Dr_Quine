759618b08878411dc48ef621d408d0362fe0ddba
Anya Schukin <anya.schukin@gmail.com>
2020-03-24 11:03:15 +0100

    WIP/saving grace_anya.c draft

---
+++ C/Grace_anya.c
@@ -0,0 +1,14 @@
+// no main
+// 3 defines
+// 1 comment
+
+/*
+** Grace reproduces herself in her image
+*/
+
+#include <stdio.h>
+#include <>
+
+# define GRACE_KID "Grace_kid.c"
+# define SOURCE ""
+# define GRACE() int main(){int fd = open(GRACE_KID, O_CREAT|O_WRONDLY|O_TRUNC, S_IRWXU); if (fd < 0) return (1), dprintf(fd, SOURCE, 10, 34, SOURCE, GRACE_KID); close(fd)}
