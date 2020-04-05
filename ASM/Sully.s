5c9d508c7c8ff134a3b2044c10303a0f9077b849
Anya Schukin <anya.schukin@gmail.com>
2020-04-05 11:46:53 +0200

    WIP/Sully creates Sully_5.s but doesnt spawn again

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -3,22 +3,20 @@
 %define SYS_OPEN                0x2000005
 
 section .data
-code: db "%4$cdefine O_CREAT_WRONLY_TRUNC    03001o%2$c%4$cdefine PERMISSIONS             644o%2$c%4$cdefine SYS_OPEN                0x2000005%2$c%2$csection .data%2$ccode: db %1$c%3$s%1$c, 0%2$cfilename: db %1$cGrace_kid.s%1$c, 0%2$c%2$c%4$cmacro RUN 0%2$c%2$csection .text%2$cglobal _main%2$cextern _dprintf%2$c%2$c_main:%2$c    push rbp%2$c    mov rbp, rsp%2$c%2$copen:%2$c    lea rdi, [rel filename]%2$c    mov rsi, O_CREAT_WRONLY_TRUNC%2$c	mov rdx, PERMISSIONS%2$c    mov rax, SYS_OPEN%2$c    syscall%2$c%2$c; duplicate boi%2$cwrite:%2$c    mov rdi, rax%2$c	lea rsi, [rel code]%2$c	mov rdx, 34%2$c	mov rcx, 10%2$c	lea r8, [rel code]%2$c    mov r9, 37%2$c	call _dprintf%2$c%2$cexit:%2$c    mov rax, 0%2$c	leave%2$c	ret%2$c%2$c%4$cendmacro%2$c%2$cRUN", 0
-; compile: db "nasm -f macho64 Sully_%1$d.s -o Sully_%1$d.o", 0
-; link:
-; execute: db "./Sully_%d", 0
+code: db "%4$cdefine O_CREAT_WRONLY_TRUNC    03001o%2$c%4$cdefine PERMISSIONS             644o%2$c%4$cdefine SYS_OPEN                0x2000005%2$c%2$csection .data%2$ccode: db %1$c%3$s%1$c, 0%2$cfilename: db %1$cSully_%d.s%1$c, 0%2$ci equ 5%2$c%2$csection .text%2$cglobal _main%2$cextern _asprintf%2$cextern _system%2$c%2$c_main:%2$c	enter 32, 0%2$c	mov r10, i%2$c	cmp r10, 0%2$c	jle exit%2$c	dec r10%2$c%2$cchild:%2$c	lea rdi, [rsp]%2$c	lea rsi, [rel filename]%2$c	mov rdx, i%2$c	call _asprintf%2$c%2$copen:%2$c    lea rdi, [rel filename]%2$c    mov rsi, O_CREAT_WRONLY_TRUNC%2$c	mov rdx, PERMISSIONS%2$c    mov rax, SYS_OPEN%2$c    syscall%2$c%2$cwrite:%2$c    mov rdi, rax%2$c	lea rsi, [rel code]%2$c	mov rdx, 34%2$c	mov rcx, 10%2$c	lea r8, [rel code]%2$c    mov r9, 37%2$c	call _asprintf%2$c%2$c; multiply boi%2$cexecute:%2$c	mov rdi, [rsp + 8]%2$c	call _system%2$c%2$cexit:%2$c    mov rax, 0%2$c	leave%2$c	ret%2$c> nasm -f macho64 Sully.s; gcc Sully.o -o Sully; ./Sully, 0", 0
 filename: db "Sully_%d.s", 0        ; define byte (1 byte)
-num: dd 5                           ; define doubleword (4 bytes)
+i equ 5                           ; define doubleword (4 bytes)
 
 section .text
 global _main
 extern _asprintf
+extern _dprintf
 extern _system
 
 _main:
 	enter 32, 0
 
-	mov r10, num
+	mov r10, i
 	cmp r10, 0
 	jle exit
 	dec r10 ; dunno if this will work
@@ -30,11 +28,11 @@ _main:
 child:
 	lea rdi, [rsp]
 	lea rsi, [rel filename]
-	mov rdx, num
+	mov rdx, i
 	call _asprintf
 
 open:
-    lea rdi, [rel filename]
+    mov rdi, [rsp]
     mov rsi, O_CREAT_WRONLY_TRUNC
 	mov rdx, PERMISSIONS
     mov rax, SYS_OPEN
@@ -47,7 +45,7 @@ write:
 	mov rcx, 10
 	lea r8, [rel code]
     mov r9, 37
-	call _asprintf
+	call _dprintf
 
 ; multiply boi
 execute:
