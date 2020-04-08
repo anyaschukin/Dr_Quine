21af63f27462d8a8043cd696c6a13cd57d8172f0
Anya Schukin <anya.schukin@gmail.com>
2020-04-08 13:53:39 +0200

    refacto/reorder NL and Quotes... Sully FINISHED

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -4,14 +4,13 @@
 %define SYS_CLOSE               0x2000006
 
 section .data
-exec: db "nasm -f macho64 Sully_%1$d.s; gcc Sully_%1$d.o -o Sully_%1$d; rm Sully_%1$d.o; ./Sully_%1$d", 0
-code: db "%%define O_CREAT_WRONLY_TRUNC    03001o%2$c%%define PERMISSIONS             644o%2$c%%define SYS_OPEN                0x2000005%2$c%%define SYS_CLOSE               0x2000006%2$c%2$csection .data%2$cexec: db %1$cnasm -f macho64 Sully_%%1$d.s; gcc Sully_%%1$d.o -o Sully_%%1$d; rm Sully_%%1$d.o; ./Sully_%%1$d%1$c, 0%2$ccode: db %1$c%3$s%1$c, 0%2$cfilename: db %1$cSully_%%d.s%1$c, 0%2$cself: db __FILE__%2$c%2$c%2$csection .text%2$cglobal _main%2$cextern _asprintf%2$cextern _dprintf%2$cextern _strchr%2$cextern _system%2$c%2$c_main:%2$c	enter 32, 0%2$c%2$c	mov r12, %4$d%2$c	cmp r12, 0%2$c	jle exit%2$c%2$c%2$ccheck:%2$c	lea rdi, [rel self]%2$c	mov rsi, '_'%2$c    call _strchr%2$c	cmp rax, 0%2$c	je child%2$c%2$c	dec r12%2$c%2$cchild:%2$c	lea rdi, [rsp]%2$c	lea rsi, [rel filename]%2$c	mov rdx, r12%2$c	call _asprintf%2$c%2$copen:%2$c    mov rdi, [rsp]%2$c    mov rsi, O_CREAT_WRONLY_TRUNC%2$c	mov rdx, PERMISSIONS%2$c    mov rax, SYS_OPEN%2$c    syscall%2$c%2$cwrite:%2$c    mov rdi, rax%2$c	lea rsi, [rel code]%2$c	mov rdx, 34%2$c	mov rcx, 10%2$c	lea r8, [rel code]%2$c    mov r9, r12%2$c	call _dprintf%2$c%2$c	mov rax, SYS_CLOSE%2$c	syscall%2$c%2$c; multiply boi%2$c%2$cexecute:%2$c	lea rdi, [rsp]%2$c	lea rsi, [rel exec]%2$c	mov rdx, r12%2$c	call _asprintf%2$c	%2$c	mov rdi, [rsp]%2$c	call _system%2$c%2$cexit:%2$c    mov rax, 0%2$c	leave%2$c	ret", 0
+self: db __FILE__, 0
 filename: db "Sully_%d.s", 0
-self: db __FILE__
+code: db "%%define O_CREAT_WRONLY_TRUNC    03001o%1$c%%define PERMISSIONS             644o%1$c%%define SYS_OPEN                0x2000005%1$c%%define SYS_CLOSE               0x2000006%1$c%1$csection .data%1$cself: db __FILE__, 0%1$cfilename: db %2$cSully_%%d.s%2$c, 0%1$ccode: db %2$c%3$s%2$c, 0%1$cexec: db %2$cnasm -f macho64 Sully_%%1$d.s; gcc Sully_%%1$d.o -o Sully_%%1$d; rm Sully_%%1$d.o; ./Sully_%%1$d%2$c, 0%1$c%1$csection .text%1$cglobal _main%1$cextern _asprintf%1$cextern _dprintf%1$cextern _strchr%1$cextern _system%1$c%1$c_main:%1$c	enter 32, 0%1$c%1$c	mov r12, %4$d%1$c	cmp r12, 0%1$c	jle exit%1$c%1$ccheck:%1$c	lea rdi, [rel self]%1$c	mov rsi, '_'%1$c    call _strchr%1$c	cmp rax, 0%1$c	je child%1$c%1$c	dec r12%1$c%1$cchild:%1$c	lea rdi, [rsp]%1$c	lea rsi, [rel filename]%1$c	mov rdx, r12%1$c	call _asprintf%1$c%1$copen:%1$c    mov rdi, [rsp]%1$c    mov rsi, O_CREAT_WRONLY_TRUNC%1$c	mov rdx, PERMISSIONS%1$c    mov rax, SYS_OPEN%1$c    syscall%1$c%1$cwrite:%1$c    mov rdi, rax%1$c	lea rsi, [rel code]%1$c	mov rdx, 10%1$c	mov rcx, 34%1$c	lea r8, [rel code]%1$c    mov r9, r12%1$c	call _dprintf%1$c%1$c	mov rax, SYS_CLOSE%1$c	syscall%1$c%1$c; multiply boi%1$cexecute:%1$c	lea rdi, [rsp]%1$c	lea rsi, [rel exec]%1$c	mov rdx, r12%1$c	call _asprintf%1$c%1$c	mov rdi, [rsp]%1$c	call _system%1$c%1$cexit:%1$c    mov rax, 0%1$c	leave%1$c	ret", 0
+exec: db "nasm -f macho64 Sully_%1$d.s; gcc Sully_%1$d.o -o Sully_%1$d; rm Sully_%1$d.o; ./Sully_%1$d", 0
 
 section .text
 global _main
-extern _printf
 extern _asprintf
 extern _dprintf
 extern _strchr
@@ -23,7 +22,7 @@ _main:
 	mov r12, 5
 	cmp r12, 0
 	jle exit
-	
+
 check:
 	lea rdi, [rel self]
 	mov rsi, '_'
@@ -49,8 +48,8 @@ open:
 write:
     mov rdi, rax
 	lea rsi, [rel code]
-	mov rdx, 34
-	mov rcx, 10
+	mov rdx, 10
+	mov rcx, 34
 	lea r8, [rel code]
     mov r9, r12
 	call _dprintf
