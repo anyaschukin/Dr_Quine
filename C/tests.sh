174cce4b6f53e5d2f29d540d54a490cc74a198b3
Anya Schukin <anya.schukin@gmail.com>
2020-06-24 17:25:40 +0200

    refacto/add C tests

--- C/tests.sh
+++ C/tests.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-echo "=========="
-echo "./Colleen"
-echo "=========="
-echo "COMMAND: ./Colleen > tmp_Colleen; diff ./Colleen.c ./tmp_Colleen"
-echo "RESULT (expect empty line):"
-./Colleen > tmp_Colleen; diff ./Colleen.c ./tmp_Colleen
-echo ""
-echo "=========="
-echo "./Grace"
-echo "=========="
-echo "COMMAND: ./Grace; diff ./Grace.c ./Grace_kid.c"
-echo "RESULT (expect empty line):"
-./Grace; diff ./Grace.c ./Grace_kid.c
+
+## COLLEEN
+clang -Wall -Wextra -Werror -o Colleen Colleen.c; ./Colleen > tmp_Colleen ; diff tmp_Colleen Colleen.c
+rm Colleen tmp_Colleen
+
+# ## GRACE
+clang -Wall -Wextra -Werror -o Grace Grace.c; ./Grace ; diff Grace.c Grace_kid.c
+rm Grace Grace_kid.c
+
+# ## SULLY
+clang -Wall -Wextra -Werror Sully.c -o Sully ; ./Sully; ls -al | grep Sully | wc -l
+rm Sully Sully_*
