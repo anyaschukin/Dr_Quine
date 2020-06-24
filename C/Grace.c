fe09a78e0356867e65afcea23c4634f0d27cc69c
Anya Schukin <anya.schukin@gmail.com>
2020-06-24 17:15:57 +0200

    refacto/use Grace_anya... works fine

--- C/Grace.c
+++ C/Grace.c
@@ -1,13 +1,13 @@
+/*
+** Grace reproduces herself in her image
+*/
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#define CONTENT "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$c#define CONTENT %3$c%2$s%3$c%1$c#define FILE %3$cGrace_kid.c%3$c%1$c#define GRACE() int main() {int fd = open(FILE, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU); if (fd < 0) return (1); dprintf(fd, CONTENT, 10, CONTENT, 34, 32, 4);close(fd);}%1$c%1$c/*%1$c%4$*5$cYou're still in the honeymoon stage. Enjoy it. 'Cause it ain't gonna last forever. - Grace%1$c*/%1$c%1$cGRACE()"
-#define FILE "Grace_kid.c"
-#define GRACE() int main() {int fd = open(FILE, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU); if (fd < 0) return (1); dprintf(fd, CONTENT, 10, CONTENT, 34, 32, 4);close(fd);}
-
-/*
-    You're still in the honeymoon stage. Enjoy it. 'Cause it ain't gonna last forever. - Grace
-*/
+# define GRACE_KID "Grace_kid.c"
+# define SOURCE "/*%1$c** Grace reproduces herself in her image%1$c*/%1$c%1$c#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$c# define GRACE_KID %2$cGrace_kid.c%2$c%1$c# define SOURCE %2$c%3$s%2$c%1$c# define GRACE() int main(){int fd = open(GRACE_KID, O_CREAT|O_WRONLY|O_TRUNC, 0644); if (fd < 0) return (1); dprintf(fd, SOURCE, 10, 34, SOURCE); close(fd);}%1$c%1$cGRACE()%1$c"
+# define GRACE() int main(){int fd = open(GRACE_KID, O_CREAT|O_WRONLY|O_TRUNC, 0644); if (fd < 0) return (1); dprintf(fd, SOURCE, 10, 34, SOURCE); close(fd);}
 
 GRACE()
