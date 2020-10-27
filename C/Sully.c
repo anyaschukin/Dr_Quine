8e0de17a4a685b87bd3f3d7890d9e4f5c3509142
Anya Schukin <anya.schukin@gmail.com>
2020-10-27 17:10:15 +0100

    refacto/fix Sully -1 trick

--- C/Sully.c
+++ C/Sully.c
@@ -7,10 +7,12 @@
 int main(){
 	int x = 5;
 
+	x < 0 ? exit(1):0;
+
 	if ((strchr(__FILE__, '_')) != NULL)
 		x--;
 
-	char *c="#include <stdio.h>%5$c#include <stdlib.h>%5$c#include <string.h>%5$c#include <fcntl.h>%5$c#include <unistd.h>%5$c%5$cint main(){%5$c%4$cint x = %3$d;%5$c%5$c%4$cif ((strchr(__FILE__, '_')) != NULL)%5$c%4$c%4$cx--;%5$c%5$c%4$cchar *c=%2$c%6$s%2$c;%5$c%4$cchar *sully;%5$c%4$cchar *filename;%5$c%4$cchar *compile;%5$c%4$cchar *execute;%5$c%4$cint f;%5$c%5$c%4$casprintf(&sully,%2$cSully_%1$cd%2$c, x);%5$c%4$casprintf(&filename,%2$cSully_%1$cd.c%2$c, x);%5$c%4$casprintf(&compile, %2$cclang -Wall -Werror -Wextra Sully_%1$c1$d.c -o Sully_%1$c1$d; %2$c, x);%5$c%4$casprintf(&execute, %2$c./Sully_%1$cd%2$c, x);%5$c%5$c%4$cf=open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);%5$c	if (f < 0)%5$c%4$c%4$cexit(1);%5$c%4$cdprintf(f,c,37,34,x,9,10,c);%5$c%4$cclose(f);%5$c%4$csystem(compile);%5$c	if (x > 0)%5$c%4$c%4$csystem(execute);%5$c%4$creturn (0);%5$c}";
+	char *c="#include <stdio.h>%5$c#include <stdlib.h>%5$c#include <string.h>%5$c#include <fcntl.h>%5$c#include <unistd.h>%5$c%5$cint main(){%5$c%4$cint x = %3$d;%5$c%5$c%4$cx < 0 ? exit(1):0;%5$c%5$c%4$cif ((strchr(__FILE__, '_')) != NULL)%5$c%4$c%4$cx--;%5$c%5$c%4$cchar *c=%2$c%6$s%2$c;%5$c%4$cchar *sully;%5$c%4$cchar *filename;%5$c%4$cchar *compile;%5$c%4$cchar *execute;%5$c%4$cint f;%5$c%5$c%4$casprintf(&sully,%2$cSully_%1$cd%2$c, x);%5$c%4$casprintf(&filename,%2$cSully_%1$cd.c%2$c, x);%5$c%4$casprintf(&compile, %2$cclang -Wall -Werror -Wextra Sully_%1$c1$d.c -o Sully_%1$c1$d; %2$c, x);%5$c%4$casprintf(&execute, %2$c./Sully_%1$cd%2$c, x);%5$c%5$c%4$cf=open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);%5$c	if (f < 0)%5$c%4$c%4$cexit(1);%5$c%4$cdprintf(f,c,37,34,x,9,10,c);%5$c%4$cclose(f);%5$c%4$csystem(compile);%5$c	if (x > 0)%5$c%4$c%4$csystem(execute);%5$c%4$creturn (0);%5$c}";
 	char *sully;
 	char *filename;
 	char *compile;
